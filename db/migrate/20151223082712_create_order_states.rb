e2149f6ed17c55c126e50609bf677728cbcbcb8b
ChengHsuanLiu <chenghsuango@gmail.com>
2015-12-23 16:27:29 +0800

    rails g model OrderState

---
+++ db/migrate/20151223082712_create_order_states.rb
@@ -0,0 +1,10 @@
+class CreateOrderStates < ActiveRecord::Migration
+  def change
+    create_table :order_states do |t|
+      t.string :name
+      t.string :code
+
+      t.timestamps null: false
+    end
+  end
+end
